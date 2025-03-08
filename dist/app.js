d3b7ebf3540c2f28b884f519a3e49fb77536238f
mirhasankhan <mirhasan000034@gmail.com>
2025-03-08 13:59:59 +0600

    dsfdf

--- dist/app.js
+++ dist/app.js
@@ -22,8 +22,6 @@ prisma
 });
 app.use((0, cors_1.default)({
     origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
